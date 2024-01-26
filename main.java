2ccf51d521a6b47f52a64e5865288f45a2b60fb9
firewill21124 <157782826+firewill21124@users.noreply.github.com>
2024-01-26 11:42:21 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,25 @@
+// one peice-1,2 clerly mentioned in insta reels
+// one peice-3
+class HelloWorld {
+    public static void main(String[] args) {
+        int start=0;
+        int end=10;
+        int offset=1;
+        for(int i=start;i<=end;i+=offset){
+            System.out.println(i); //print statement to display content in new line
+        }
+    }
+}
 
+// explination
+// Here we declared 3 cariables of integer data type and started for loop, for loop will take 3 arguments they are start value, end value and offset value 
+
+// offset value refers to the skip value
+// example:
+// start=1
+// end=10
+// offset=5
+
+// so output will be:
+// 1
+// 6
