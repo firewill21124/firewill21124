2afa5b1c02e3bfaefa3f7d515b4b35b41d8fb926
firewill21124 <157782826+firewill21124@users.noreply.github.com>
2024-01-30 21:55:07 +0530

    Update main.java

--- main.java
+++ main.java
@@ -23,3 +23,18 @@ class HelloWorld {
 // so output will be:
 // 1
 // 6
+
+// One Piece-4
+class HelloWorld {
+    public static void main(String[] args) {
+        int start=0;
+        int end=10;
+        int offset=1;
+        for(start<=end){
+            System.out.println(start); //print statement to display content in new line
+            start+=offset;
+        }
+    }
+}
+
+// it is same as for loop but with experience we will know where to use while loop and for loop
